4a9d3d699cb5b574de92a2a22536a6235e9ea962
miya <migcornbread@gmail.com>
2016-10-09 15:56:02 -0400

    new changes to import bp0

--- bp0.py
+++ bp0.py
@@ -26,66 +26,54 @@ class BpZeroLayer(Convolution2D):
     def __init__(self, filter_index, input_filters):
         super(BpZeroLayer, self).__init__(16, 1,  1, trainable=False, border_mode='same', weights=[np.array([ [ [[1]] if i == filter_index else [[0]] for i in range(0,input_filters) ] for j in range(0, input_filters)]), np.zeros(input_filters)])
 
-class BpOneLayer(Convolution2D):
-    '''
-    Make a 2d convolution that can be swapped for a BpZeroLayer
-    '''
-    def __init__(self, input_filters):
-        super(BpOneLayer, self).__init__(input_filters, 1, 1, trainable=False, init='one')
-ex_model = load_model('example_model.h5')
+def mnist_setup(filename):
+    ex_model = load_model('example_model.h5')
 
-batch_size = 128
-nb_classes = 10  # 10 digits from 0 to 9
+    batch_size = 128
+    nb_classes = 10  # 10 digits from 0 to 9
 
-# input image dimensions
-img_rows, img_cols = 28, 28
-# number of convolutional filters to use
-nb_filters = 32
+    # input image dimensions
+    img_rows, img_cols = 28, 28
+    # number of convolutional filters to use
+    nb_filters = 32
 
-# the data, shuffled and split between tran and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+    # the data, shuffled and split between tran and test sets
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Reshape data
-X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-X_train = X_train.astype("float32")
-X_test = X_test.astype("float32")
-X_train /= 255
-X_test /= 255
-print('X_train shape:', X_train.shape)
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
+    # Reshape data
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    X_train = X_train.astype("float32")
+    X_test = X_test.astype("float32")
+    X_train /= 255
+    X_test /= 255
 
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+    # convert class vectors to binary class matrices
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    return ex_model, X_train, Y_train, X_test, Y_test
+        
+def bp_visualize(model_setup, filename):
+    ex_model, X_train, Y_train, X_test, Y_test = model_setup(filename)
+    # for every filter
+    X_max = []
+    for k in list(range(16)):
+        model = Sequential()
+        inp = 4
+        for i in range(0,inp):
+            model.add(ex_model.layers[i])
+        model.add(BpZeroLayer(k, 16))
+        for i in range(inp,  len(ex_model.layers)):
+            model.add(ex_model.layers[i])
+        #model.layers[inp].set_weights(BpZeroLayer(i, 16).get_weights())
+        # for every piece of data
+        min_discrepancy = [float("inf") for i in range(0,9)]
+        X_max.append([0 for i in range(0,9)])
+        for j in list(range(400)):#X_train.shape[0])):
+            disc = np.dot(model.predict_on_batch(np.array([X_train[j]])), Y_train[j])
+            if max(min_discrepancy) > disc[0]:
+                min_discrepancy[np.argmax(min_discrepancy)] = disc[0]
+                X_max[k][np.argmax(min_discrepancy)] = j
+        del model
+    return X_max
 
-# for every filter
-X_max = []
-for k in list(range(16)):
-    print("We're on filter:", k)
-    model = Sequential()
-    inp = 4
-    for i in range(0,inp):
-        model.add(ex_model.layers[i])
-    model.add(BpZeroLayer(k, 16))
-    print("in the middle:")
-    print(len(model.layers[inp].get_weights()[0]))
-    print(len(model.layers[inp].get_weights()[0][0]))
-    print(len(model.layers[inp].get_weights()[0][0][0]))
-    print(len(model.layers[inp].get_weights()[0][0][0][0]))
-    for i in range(inp,  len(ex_model.layers)):
-        model.add(ex_model.layers[i])
-    #model.layers[inp].set_weights(BpZeroLayer(i, 16).get_weights())
-    # for every piece of data
-    min_discrepancy = [float("inf") for i in range(0,9)]
-    X_max.append([0 for i in range(0,9)])
-    for j in list(range(400)):#X_train.shape[0])):
-        print(j)
-        disc = np.dot(model.predict_on_batch(np.array([X_train[j]])), Y_train[j])
-        print(disc[0])
-        if max(min_discrepancy) > disc[0]:
-            min_discrepancy[np.argmax(min_discrepancy)] = disc[0]
-            X_max[k][np.argmax(min_discrepancy)] = j
-    del model
-print(X_max)
