9ba682e24d23eb065e05dfb65d7ed26081364fbf
Ryan Baylor Killea <rbkillea@wpi.edu>
2016-10-09 16:05:57 -0400

    saliencymap handoff

---
+++ saliencymaps.py
@@ -0,0 +1,23 @@
+from keras import backend as K
+
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(123)
+
+from keras import backend as K
+from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, Convolution1D, MaxPooling2D
+from keras.utils import np_utils
+import * from bp0
+import pprint
+import inspect
+K.set_image_dim_ordering('th')
+
+model, X_train = mnist_setup('example_model.h5')[:2]
+
+l = 10
+filt = 2
+K.function([model.layers[0].input, 0], [model.layers[l].output])
+vis_layer = BpZeroLayer(
