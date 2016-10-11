9c4285b51a25139ca21e8b705ce85a7942ffb860
Ryan Baylor Killea <rbkillea@wpi.edu>
2016-10-11 00:34:53 -0400

    a slightly error-happy but working deconv

--- deconv.py
+++ deconv.py
@@ -30,17 +30,18 @@ class Unpooling2D(Layer):
         self.poolsize = poolsize
         self.ignore_border = ignore_border
 
-    def get_output(self, train):
-        X = self.get_input(train)
+    def call(self, x, mask=None):
         s1 = self.poolsize[0]
         s2 = self.poolsize[1]
-        output = X.repeat(s1, axis=2).repeat(s2, axis=3)
+        output = x.repeat(s1, axis=2).repeat(s2, axis=3)
         return output
 
     def get_config(self):
         return {"name":self.__class__.__name__,
             "poolsize":self.poolsize,
             "ignore_border":self.ignore_border}
+    def get_output_shape_for(self, input_shape):
+        return (input_shape[0], input_shape[1], input_shape[2]*self.poolsize[0], input_shape[3]*self.poolsize[1])
 
 ex_model, X_train, Y_train = mnist_setup('example_model.h5')[:3]
 models = []
@@ -53,12 +54,12 @@ for i in range(0, len(ex_model.layers)):
         l2 = ex_model.layers[j]
         while not isinstance(l2, Activation):
             if isinstance(l2, Convolution2D):
-                print([l2.nb_filter, l2.nb_row, l2.nb_col, l2.input_shape, l2.output_shape[-3:]])
-                model2.add(Deconvolution2D(nb_filter=l2.nb_filter,
+                print([l2.nb_filter, l2.nb_row, l2.nb_col, l2.input_shape[-3:], l2.output_shape[-3:]])
+                model2.add(Deconvolution2D(nb_filter=l2.input_shape[-3],
                                            nb_row=l2.nb_row,
                                            nb_col=l2.nb_col,
                                            output_shape=(l2.input_shape[-3:]),
-                                           input_shape=(l2.output_shape[-3:])))
+                                           input_shape=(l2.output_shape[-3:]), border_mode='same'))
             elif isinstance(l2, MaxPooling2D):
                 model2.add(Unpooling2D(l2.pool_size))
             j = j - 1
@@ -76,14 +77,21 @@ with open('activation_data.pickle', 'rb') as handle:
 
 batch_size = 16
 
-print(b[0][0])
-
 #train (taken from example_keras_cnn.py)
 for model in list(enumerate(models)):
     # mse = l2 loss function
+    print(model[0])
     model[1].compile(loss='mse', optimizer = 'adadelta')
     nb_epoch = 2  # try increasing this number
-    model[1].fit(np.squeeze(b[model[0]+1]), np.squeeze(b[model[0]]), batch_size=batch_size, nb_epoch=nb_epoch,
-          show_accuracy=True, verbose=1)
-
+    up = np.array(b[model[0]+1])
+    down = np.array(b[model[0]])
+    print(up.shape)
+    print(down.shape)
+    print(model[1].layers[0].input_shape)
+    print(model[1].layers[-1])
+    print(model[1].layers[-1].output_shape)
+    model[1].fit(np.reshape(up, (up.shape[0],up.shape[-3],up.shape[-2],up.shape[-1])),
+                 np.reshape(down, (down.shape[0],down.shape[-3],down.shape[-2],down.shape[-1])), batch_size=batch_size, nb_epoch=nb_epoch, show_accuracy=True, verbose=1)
+    model[1].evaluate(np.reshape(up, (up.shape[0],up.shape[-3],up.shape[-2],up.shape[-1])),
+                      np.reshape(down, (down.shape[0],down.shape[-3],down.shape[-2],down.shape[-1])), show_accuracy=True, verbose = 0)
 
