93d8235253db4a5c9f6524b3148e13b07ffd0ef8
Ryan Baylor Killea <rbkillea@wpi.edu>
2016-10-11 13:10:50 -0400

    make visualizations work

--- saliencymaps.py
+++ saliencymaps.py
@@ -18,8 +18,7 @@ K.set_image_dim_ordering('th')
 model, X_train = mnist_setup('example_model.h5')[:2]
 
 l = 5
-filt = 2
-image_in = 0
+filt = 4
 sal_model = Sequential()
 pred_model = Sequential()
 for layer in model.layers[:l]:
@@ -30,17 +29,18 @@ sal_model.add(BpZeroLayer(filt, model.layers[l].output_shape[1]))
 def silly_loss(y_true, y_pred):
     return K.ones_like(y_true)
 
-weights_before = sal_model.layers[0].get_weights()[0]
-print(weights_before)
-sal_model.compile(loss='mse', optimizer='adadelta')
-sal_model.fit(np.array([X_train[image_in]]), pred_model.predict(np.array([X_train[image_in]])), batch_size=1, nb_epoch=1, show_accuracy=True, verbose=1)
-filt_result = np.mean(np.squeeze(sal_model.layers[0].get_weights()[0] - weights_before), axis=0)
-filt_result /= np.linalg.norm(filt_result)
-result = convolve2d(np.squeeze(X_train[image_in]), filt_result)
-print(filt_result)
-print(result.shape)
 plt.figure(figsize=(10,10))
-plt.subplot(1,1,1)
-plt.imshow(result, cmap='gray')
-plt.xlabel("result of saliency map")
+weights_before = sal_model.layers[0].get_weights()[0]
+sal_model_snapshot = Sequential()
+sal_model_snapshot.add(sal_model)
+for image_in in range(9):
+    sal_model.compile(loss='mse', optimizer='adadelta')
+    sal_model.fit(np.array([X_train[image_in]]), pred_model.predict(np.array([X_train[image_in]])), batch_size=1, nb_epoch=3, show_accuracy=True, verbose=1,sample_weight=np.array([10]))
+    filt_result = np.mean(np.squeeze(sal_model.layers[0].get_weights()[0] - weights_before), axis=0)
+    filt_result /= np.linalg.norm(filt_result)
+    result = convolve2d(np.squeeze(X_train[image_in]), filt_result)
+    sal_model = sal_model_snapshot
+    plt.subplot(3,3,image_in + 1)
+    plt.imshow(result, cmap='gray')
+    plt.xlabel("result of saliency map")
 plt.show()
